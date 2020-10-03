d5d7e43f33ee775e48282c959241f5363d051960
AhmadMuafi <ahmadmuafialfian@gmail.com>
2020-10-03 14:12:49 +0700

    versi 2.00

---
+++ js/dom.js
@@ -0,0 +1,9 @@
+document.querySelector("h1").innerText = "saya belajar javascript";
+
+document.querySelector("p").innerText = "Belajar javascript itu mudah";
+
+document.querySelector(".isi").innerText =
+  "saya belajar javascript di smkn 2 buduran";
+
+document.querySelector("#judul").innerText =
+  "belajar javascript itu menyenangkan";
